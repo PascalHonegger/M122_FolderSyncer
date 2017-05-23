6e005b6b451f551048c8573e3563c0d63e64b183
Pascal Honegger <pascal.honegger@edu.tbz.ch>
2017-05-23 10:55:06 +0200

    Progress anzeigen. Auf anderem Thread ausführen, damit GUI nicht blockiert

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
 import os
-from tkinter import Tk, StringVar
+from tkinter import Tk, StringVar, DoubleVar
 from tkinter.ttk import Button, Label, Progressbar, Entry
 from tkinter.filedialog import askdirectory
 from tkinter.messagebox import showinfo
 from filecmp import cmp
 from shutil import copy2
 
+import _thread
+
 window = Tk()
 
 window.title("Folder syncer")
-window.geometry("525x75")
+window.geometry("525x100")
 
 source_label = Label(window, text="Source folder:")
 source_label.grid(row=0, column=0, columnspan=2)
@@ -44,6 +46,10 @@ chose_target_button = Button(window, command=chose_target_folder, text="Get targ
 chose_target_button.grid(row=1, column=4)
 
 
+def sync_folders_new_thread():
+    _thread.start_new_thread(sync_folders, ())
+
+
 def sync_folders():
     if not source_path.get():
         showinfo("Source folder required", "Please enter a source folder")
@@ -62,6 +68,12 @@ def sync_folders():
         return
 
     all_source_files = []
+    all_target_files = []
+    all_added_files = []
+    all_deleted_files = []
+    all_changed_files = []
+    all_existing_files = []
+    progress_value.set(0)
 
     for dirpath, dirnames, filenames in os.walk(source_path.get()):
         for fileName in filenames:
@@ -69,19 +81,11 @@ def sync_folders():
             relative_path = os.path.relpath(full_path, source_path.get())
             all_source_files.append(relative_path)
 
-    all_target_files = []
-
     for dirpath, dirnames, filenames in os.walk(target_path.get()):
         for fileName in filenames:
             full_path = os.path.join(dirpath, fileName)
             relative_path = os.path.relpath(full_path, target_path.get())
             all_target_files.append(relative_path)
-
-    all_added_files = []
-    all_deleted_files = []
-    all_changed_files = []
-
-    all_existing_files = []
 
     for source_file in all_source_files:
         if source_file in all_target_files:
@@ -100,17 +104,19 @@ def sync_folders():
         if not cmp(full_source_path, full_target_path):
             all_changed_files.append(existing_file)
 
-    #TODO compare content
-    print(all_added_files)
-    print(all_existing_files)
-    print(all_changed_files)
-    print(all_deleted_files)
+    added_label.config(text="Added " + str(len(all_added_files)) + " files")
+    changed_label.config(text="Changed " + str(len(all_changed_files)) + " files")
+    deleted_label.config(text="Deleted " + str(len(all_deleted_files)) + " files")
 
     to_be_copied = all_added_files + all_changed_files
 
-    # TODO check folder structure
+    total_changed_items = len(to_be_copied + all_deleted_files);
 
-    step = 100 / len(to_be_copied + all_deleted_files)
+    if total_changed_items == 0:
+        progress_value.set(100)
+        return
+
+    step = 100 / total_changed_items
 
     for changed_file in to_be_copied:
         full_source_path = os.path.join(source_path.get(), changed_file)
@@ -118,20 +124,29 @@ def sync_folders():
         if not os.path.exists(os.path.dirname(full_target_path)):
             os.makedirs(os.path.dirname(full_target_path))
         copy2(full_source_path, full_target_path)
-        progress.step(step)
-        progress.update()
+        progress_value.set(progress_value.get() + step)
 
     for deleted_file in all_deleted_files:
         full_target_path = os.path.join(target_path.get(), deleted_file)
         os.remove(full_target_path)
-        progress.step(step)
-        progress.update()
+        progress_value.set(progress_value.get() + step)
 
+    progress_value.set(100)
 
-sync_button = Button(window, command=sync_folders, text="<- Sync ->")
+sync_button = Button(window, command=sync_folders_new_thread, text="<- Sync ->")
 sync_button.grid(row=1, column=2)
 
-progress = Progressbar(window, maximum=100, mode="determinate", length=525)
+progress_value = DoubleVar()
+progress = Progressbar(window, maximum=100, mode="determinate", length=525, variable=progress_value)
 progress.grid(row=2, column=0, columnspan=5)
+
+added_label = Label(window, text="Added 0 files", foreground="green", justify="left")
+added_label.grid(row=3, column=0)
+
+changed_label = Label(window, text="Changed 0 files", foreground="orange", justify="center")
+changed_label.grid(row=3, column=2)
+
+deleted_label = Label(window, text="Deleted 0 files", foreground="red", justify="right")
+deleted_label.grid(row=3, column=4)
 
 window.mainloop()
